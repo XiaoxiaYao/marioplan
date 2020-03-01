dac1238559f12a6b2b6e7b62b8b652426cb5d199
XiaoxiaYao <michaelyaonz@gmail.com>
2020-03-01 11:59:45 +0800

    ProjectList is done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
+import Dashboard from "./components/dashboard/Dashboard";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Navbar />
+        <Switch>
+          <Route path="/" component={Dashboard} />
+        </Switch>
       </BrowserRouter>
     </div>
   );
