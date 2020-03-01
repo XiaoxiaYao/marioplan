f0553dfdb773ebe924092871597e31ae7271e7d6
XiaoxiaYao <michaelyaonz@gmail.com>
2020-03-01 11:49:36 +0800

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import "./App.css";
 import { BrowserRouter } from "react-router-dom";
+import Navbar from "./components/layout/Navbar";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <BrowserRouter>
+        <Navbar />
+      </BrowserRouter>
     </div>
   );
 }
