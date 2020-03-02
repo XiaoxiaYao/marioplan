7236b93096ab8e579b9b0c4d47a107345f26e0cc
XiaoxiaYao <michaelyaonz@gmail.com>
2020-03-02 22:09:23 +0800

    Adding dummy data

--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -1,15 +1,12 @@
 import React from "react";
 import ProjectSummary from "./ProjectSummary";
 
-const ProjectList = () => {
-  return (
-    <div className="project-list section">
-      <ProjectSummary />
-      <ProjectSummary />
-      <ProjectSummary />
-      <ProjectSummary />
-    </div>
-  );
+const ProjectList = ({ projects }) => {
+  const projectSummaries = projects.map(project => {
+    return <ProjectSummary project={project} key={project.id} />;
+  });
+
+  return <div className="project-list section">{projectSummaries}</div>;
 };
 
 export default ProjectList;
--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -1,4 +1,10 @@
-const initState = {};
+const initState = {
+  projects: [
+    { id: "1", title: "Help1", content: "sdfdsfsdfsdf" },
+    { id: "2", title: "Help2", content: "sdfdsfsdfsdf" },
+    { id: "3", title: "Help3", content: "sdfdsfsdfsdf" }
+  ]
+};
 
 const projectReducer = (state = initState, action) => {
   return state;
